e058875e9896e6013501c4796f9d2c3cd0b697ce
Kyler Harden <kwharden71@gmail.com>
2025-08-10 13:56:55 -0700

    Fixed Custom Domain not remaining on deploy. Also fixed issue with 404.html reloading forever instead of letting react router handle path.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import  ReactRouter from './ReactRouter';
 
+// Add this at the very top of your entry file (e.g., src/index.tsx)
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get('redirect');
+if (redirect) {
+  window.history.replaceState(null, '', redirect);
+}
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
